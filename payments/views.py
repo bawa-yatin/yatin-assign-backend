bc0bbfbdd1ff1badd86263b04d442c6661b8ba42
bawa-yatin <Yatin.bawa@mindbowser.com>
2022-07-22 18:03:19 +0530

    Integrated Stripe API for making payments and displayed proper messages

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.shortcuts import redirect
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import stripe
 
-# Create your views here.
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+class StripeCheckoutView(APIView):
+    def post(self, request):
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                line_items=[
+                    {
+                        'price': 'price_1LOFkRSGJZ7JUrEoLBidJ9GK',
+                        'quantity': 1,
+                    },
+                ],
+                payment_method_types=['card', ],
+                mode='payment',
+                success_url=settings.SITE_DOMAIN + '/?success=true&session_id={CHECKOUT_SESSION_ID}',
+                cancel_url=settings.SITE_DOMAIN + '/?canceled=true',
+            )
+
+            return redirect(checkout_session.url)
+        except:
+            return Response({"error": "Something went wrong while creating payment checkout session"},
+                            status=status.HTTP_500_INTERNAL_SERVER_ERROR
+                            )
--- stripe_payments/settings.py
+++ stripe_payments/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-sh3#kkn3op3e=g%8+r-3dkxd3i#se!n%32mi*mclu9h_)zip9u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,16 +35,45 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'corsheaders',
+    'payments'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 ROOT_URLCONF = 'stripe_payments.urls'
@@ -69,7 +96,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'stripe_payments.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -79,7 +105,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -99,7 +124,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -111,11 +135,20 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ]
+}
+
+SITE_DOMAIN = 'http://localhost:3000'
+
+STRIPE_SECRET_KEY = 'sk_test_51LNZ7iSGJZ7JUrEoq1t2uLiW9NcYakqFcbeYXKLpAOJBJGcOsSPQUThgatKMS8WJ19MvM82bmbWRtc1UdmE0n8vf00TZZAwvCQ'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
