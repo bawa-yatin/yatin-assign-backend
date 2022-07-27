b60898524a60ec14ed3ca63bad18cea5f9b35284
bawa-yatin <Yatin.bawa@mindbowser.com>
2022-07-27 15:50:29 +0530

    Minor changes done in views file

--- payments/views.py
+++ payments/views.py
@@ -1,15 +1,18 @@
+import os
+import stripe
 from django.conf import settings
-from django.shortcuts import redirect
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import ProductDetails
-import stripe, os
 
 # Stripe secret key getting fetched from settings.py file
 stripe.api_key = settings.STRIPE_SECRET_KEY
+
 # Product price id getting fetched from .env file
 price_id = os.environ.get('PRICE_ID')
 
@@ -25,9 +28,8 @@ class StripeCheckoutView(APIView):
         product_name = data['product']
 
         try:
-            # Creation of checkout session consisting of  line items (having price and
-            # quantity), and currency, and acceptable payment methods which customer will
-            # see on checkout page.
+            # Creation of checkout session consisting of  line items,currency, and
+            # acceptable payment methods which customer will see on checkout page.
             checkout_session = stripe.checkout.Session.create(
                 line_items=[
                     {
@@ -58,31 +60,30 @@ class StripeCheckoutView(APIView):
 # completes checkout
 @csrf_exempt
 def stripe_webhook(request):
-    payload = request.body  # payload contains all the data that is returned after
+    payload = request.body  # payload contains the event that is returned after
     # successful payment
+
     sig_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
 
     # Verifying the webhook that came from stripe using the payload, Stripe signature,
-    # and our Stripe webhook secret. If the webhook is verified we can then access
+    # and Stripe webhook secret. If the webhook is verified we can then access
     # the data from the event
     try:
         event = stripe.Webhook.construct_event(
             payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
         )
-    except ValueError as e:
+    except ValueError:
         # Invalid payload
         return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
+
+    except stripe.error.SignatureVerificationError:
         # Invalid signature
         return HttpResponse(status=400)
 
     # Handle the checkout.session.completed event
     if event['type'] == 'checkout.session.completed':
         session = event['data']['object']
-
-        # If payment is successful, we send the session completed event object inside
-        # create_order function which ultimately save product order details in database
         create_order(session)
 
     # Passed signature verification
